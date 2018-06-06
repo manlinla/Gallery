b31d01edb53aa1f611f33e48390f4a6dd5231701
manlinla <mld@berkeley.edu>
2018-06-06 14:14:01 -0700

    refactor, styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../assets/logo.svg';
 
 export class Header extends React.Component {
     render() {
         return (
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Around</h1>
+                <h1 className="App-title">Gallery</h1>
             </header>
         );
     }
