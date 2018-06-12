67e7474f401b0e6322098a21bd02ae6426b33520
manlinla <mld@berkeley.edu>
2018-06-11 17:12:05 -0700

    login logics

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Register } from './Register';
-import { Login} from "./Login";
+//import { Login} from "./Login";
+import { Switch, Route } from 'react-router-dom';
+//import { Home } from './Home';
+
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Register/>
-                <Login/>
+                <Switch>
+                    <Route exact path="/" render={this.getRoot}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/login" render={this.getLogin}/>
+                    <Route path="/home" render={this.getHome}/>
+                    <Route render={this.getRoot}/>
+                </Switch>
             </div>
         );
     }
