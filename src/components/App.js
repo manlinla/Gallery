ad05bf3842611aa3692848d4fca4057402dd921a
manlinla <mld@berkeley.edu>
2018-06-12 16:39:22 -0700

    logout

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import { Header } from './Header';
 import { Main } from './Main';
+import { TOKEN_KEY } from '../constants';
 
 import '../styles/App.css';
 
 class App extends Component {
-  render() {
+
+    state = {
+        isLoggedIn: !!localStorage.getItem(TOKEN_KEY),
+    }
+
+    handleLogin = (response) => {
+        localStorage.setItem(TOKEN_KEY, response);
+        this.setState({ isLoggedIn: true });
+    }
+
+    handleLogout = () => {
+        localStorage.removeItem(TOKEN_KEY);
+        this.setState({ isLoggedIn: false });
+    }
+
+    render() {
     return (
       <div className="App">
-          <Header/>
-          <Main/>
+          <Header isLoggedIn={this.state.isLoggedIn} handleLogout={this.handleLogout}/>
+          <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin}/>
       </div>
 
     );
