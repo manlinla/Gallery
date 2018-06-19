ed3607608f5f79565d21baa391c91a2a3ef16382
manlinla <mld@berkeley.edu>
2018-06-19 14:52:35 -0700

    load geolocation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import { GEO_OPTIONS } from '../constants';
+
 
 export class Home extends React.Component {
+    componentDidMount() {
+        this.getGeoLocation();
+    }
+
+    getGeoLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS,
+            );
+        } else {
+            // geo location is not available
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+    }
+
+    onFailedLoadGeoLocation = (error) => {
+        console.log(error);
+    }
+
     render() {
         return (
             <div>Welcome Home Bud</div>
