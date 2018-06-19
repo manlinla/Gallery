dfb0ba5fe1c99fc6b9bad20c9688d30d333cce14
manlinla <mld@berkeley.edu>
2018-06-19 16:41:46 -0700

    conditional render at loading geolocation

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,13 @@ const TabPane = Tabs.TabPane;
 
 
 export class Home extends React.Component {
+    state = {
+        loadingGeoLocation: false,
+        // state management: start with false, set to true after loaded
+    }
+
     componentDidMount() {
+        this.setState({loadingGeoLocation: true});
         this.getGeoLocation();
     }
 
@@ -23,20 +29,25 @@ export class Home extends React.Component {
     }
 
     onSuccessLoadGeoLocation = (position) => {
+        this.setState({loadingGeoLocation: false});
         console.log(position);
     }
 
     onFailedLoadGeoLocation = (error) => {
+        this.setState({loadingGeoLocation: false});
         console.log(error);
     }
 
+    // conditional render
     render() {
         const operations = <Button type="primary">Create New Post</Button>;
 
         return (
             <div>
                 <Tabs tabBarExtraContent={operations}>
-                    <TabPane tab="Gallery" key="1">Content of tab 1</TabPane>
+                    <TabPane tab="Gallery" key="1">
+                        {this.state.loadingGeoLocation ? 'Loading Geo Location' : ''}
+                    </TabPane>
                     <TabPane tab="Map" key="2">Content of tab 2</TabPane>
                 </Tabs>
             </div>
