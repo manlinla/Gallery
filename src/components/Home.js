7809d01e8eb28ba8f9290e113c7ab282a730e4e0
manlinla <mld@berkeley.edu>
2018-06-25 18:22:32 -0700

    error handle

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {API_ROOT, GEO_OPTIONS, POS_KEY, AUTH_PREFIX, TOKEN_KEY} from '../constants';
-import { Tabs, Button } from 'antd';
+import { Tabs, Button, Spin } from 'antd';
 import $ from 'jquery';
 
 const TabPane = Tabs.TabPane;
@@ -10,15 +10,16 @@ export class Home extends React.Component {
     state = {
         loadingGeoLocation: false,
         // state management: start with false, set to true after loaded
-        //loadingPost: false
+        loadingPosts: false,
+        error: '',
     }
 
     componentDidMount() {
-        this.setState({loadingGeoLocation: true});
         this.getGeoLocation();
     }
 
     getGeoLocation = () => {
+        this.setState({loadingGeoLocation: true, error: ''}); // reset error when load successfully
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(
                 this.onSuccessLoadGeoLocation,
@@ -26,12 +27,12 @@ export class Home extends React.Component {
                 GEO_OPTIONS,
             );
         } else {
-            // geo location is not available
+            this.setState({ error: 'geo location is not supported D:'});
         }
     }
 
     onSuccessLoadGeoLocation = (position) => {
-        this.setState({loadingGeoLocation: false});
+        this.setState({loadingGeoLocation: false, error: ''});
         console.log(position);
         const { latitude, longtitude } = position.coords;
         localStorage.setItem(POS_KEY, JSON.stringify({ lat: latitude, lon: longtitude}));
@@ -39,11 +40,12 @@ export class Home extends React.Component {
     }
 
     onFailedLoadGeoLocation = (error) => {
-        this.setState({loadingGeoLocation: false});
+        this.setState({loadingGeoLocation: false, error: 'Failed to load geo location D:'});
         console.log(error);
     }
 
     loadNearbyPosts = () => {
+        this.setState({ loadingPosts: true , error: ''});
         const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
         $.ajax({
             url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=2000`,
@@ -52,14 +54,30 @@ export class Home extends React.Component {
                 Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
             },
         }).then((response) => {
+            this.setState({ loadingPosts: false, error: '' });
             console.log(response);
             }, (response) => {
+            this.setState({ loadingPosts: false , error: 'Failed to load posts D:'});
             console.log(response.responseText);
         }).catch((error) => {
             console.log(error);
         });
     }
 
+    getGalleyPanelContent = () => {
+        if (this.state.error) { //this.state!!
+            return <div>
+                {this.state.error};
+            </div>
+        }
+        else if (this.state.loadingGeoLocation) {
+            return <Spin tip="Loading Geo Location"/>;
+        } else if (this.state.loadingPosts) {
+            return <Spin tip="Loading Posts"/>;
+        } else {
+            return '';
+        }
+    }
     // conditional render
     render() {
         const operations = <Button type="primary">Create New Post</Button>;
@@ -68,7 +86,7 @@ export class Home extends React.Component {
             <div>
                 <Tabs tabBarExtraContent={operations}>
                     <TabPane tab="Gallery" key="1">
-                        {this.state.loadingGeoLocation ? 'Loading Geo Location' : ''}
+                        {this.getGalleyPanelContent()}
                     </TabPane>
                     <TabPane tab="Map" key="2">Content of tab 2</TabPane>
                 </Tabs>
